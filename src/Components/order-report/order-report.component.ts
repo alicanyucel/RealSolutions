27f0f85b73ad7967510d36fe3036aaa4eb9a51ef
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-10 23:00:46 +0300

    componentler eklendi

---
+++ src/Components/order-report/order-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-report',
+  templateUrl: './order-report.component.html',
+  styleUrls: ['./order-report.component.css']
+})
+export class OrderReportComponent {
+
+}
